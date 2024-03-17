41b06801b8fecac1532e6fcbf42263407b51edce
Adam Gray <adamgrayscale@gmail.com>
2024-03-16 19:21:02 -0500

    basic profile page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,14 @@
+import Nav from '@/components/Nav';
+import { useAuth } from '@/utils/AuthContext';
+import React from 'react';
+
+export default function Profile() {
+  const { user } = useAuth();
+
+  return (
+    <div className='container mx-auto px-8'>
+      <Nav/>
+      <h1 className='font-bold text-2xl'>{user?.email}</h1>
+    </div>
+  );
+};
