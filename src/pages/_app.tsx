aa9516f0ce85f3c71eeb28d5f42004c6d7ea5bc4
Adam Gray <adamgrayscale@gmail.com>
2024-03-17 12:33:18 -0500

    remove language context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import "@/styles/globals.css";
 import { AuthProvider } from "@/utils/AuthContext";
-import { LanguageProvider } from "@/utils/LanguageContext";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <LanguageProvider>
-        <Component {...pageProps} />
-      </LanguageProvider>
+      <Component {...pageProps} />
     </AuthProvider>
   );
 }
