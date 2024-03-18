3b60c9d80542281bbf386ebeb122750153a57b4d
Adam Gray <adam.gray@netlify.com>
2024-03-18 14:38:01 -0500

    prettier image

--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -10,7 +10,7 @@ export default function ProductPage({ product }: { product: Product }) {
     <main className="container mx-auto px-8">
       <Nav />
       <div className='grid md:grid-cols-2 gap-20'>
-        {product.imgSrc && <img src={`/.netlify/images?url=${product?.imgSrc}&q=50`}  alt={product.name} />}
+        {product.imgSrc && <img src={`/.netlify/images?url=${product?.imgSrc}&q=50`}  alt={product.name} className='rounded-xl shadow-xl'/>}
         <div>
           <h1 className='font-bold text-4xl mb-8'>{product.name}</h1>
           <h2 className='text-3xl'>{product.memberDiscount && user ? (
