be2341b217ff2064ca9c9c4db447e25c3d4f56ce
Adam Gray <adam.gray@netlify.com>
2024-03-18 14:42:37 -0500

    more stylin

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -8,27 +8,25 @@ export default function Cart() {
   return (
     <main className="container mx-auto px-8">
       <Nav />
-      <div className="mx-auto max-w-[75ch]">
-        <div className="flex justify-between items-center">
+      <div className="mx-auto max-w-[75ch] pt-8">
+        <div className="flex justify-between items-center border-b">
           <h2 className="font-bold text-3xl mb-8">Your Cart</h2>
           <p className="text-xl">{totalCartItems} items</p>
         </div>
-        <div className="py-4 grid grid-cols-3 border-b">
-          <span>Item</span>
-          <span>Price</span>
-          <span></span>
-        </div>
         {cartItems.map((item) => (
-          <div key={item.id} className="py-4 grid grid-cols-3">
-            <div>{item.name}</div>
-            <div><Currency />{item.price}</div>
-            <p onClick={() => removeFromCart(item.id)} className="underline">Remove</p>
+          <div key={item.id} className="py-4 grid grid-cols-2 items-center">
+            <div>
+              <span className="font-bold">{item.name}</span>
+              <button onClick={() => removeFromCart(item.id)} className="block underline">Remove</button>
+            </div>
+            <div className="flex justify-end gap-4">
+              <span><Currency />{item.price}</span>
+            </div>
           </div>
         ))}
-        <div className="py-4 grid grid-cols-3 border-t">
+        <div className="py-4 grid grid-cols-2 border-t">
           <span></span>
-          <h3 className="font-bold text-2xl"><Currency />{totalCartPrice}</h3>
-          <span></span>
+          <h3 className="font-bold text-2xl text-right"><Currency />{totalCartPrice}</h3>
         </div>
       </div>
     </main>
