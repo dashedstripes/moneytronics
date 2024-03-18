f6bf66f3363817e59fcfc4b1fc89297fbf8b3dcb
Adam Gray <adam.gray@netlify.com>
2024-03-18 11:43:13 -0500

    kick out /profile if user isnt logged in

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -5,6 +5,15 @@ import React from 'react';
 export default function Profile() {
   const { user } = useAuth();
 
+  if(!user) {
+    return (
+      <div className='container mx-auto px-8'>
+        <Nav/>
+        <div>You do not have a profile</div>
+      </div>
+    );
+  }
+
   return (
     <div className='container mx-auto px-8'>
       <Nav/>
