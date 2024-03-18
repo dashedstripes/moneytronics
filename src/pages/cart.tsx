e73a3f342e720c74eb2bd2a1faef7f7e0b1d7057
Adam Gray <adam.gray@netlify.com>
2024-03-18 13:32:28 -0500

    prettify cart and remove items

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -3,22 +3,32 @@ import Nav from "@/components/Nav";
 import { useCart } from "@/utils/CartContext";
 
 export default function Cart() {
-  const { cartItems, totalCartPrice } = useCart();
+  const { cartItems, totalCartItems, totalCartPrice, removeFromCart } = useCart();
 
   return (
     <main className="container mx-auto px-8">
       <Nav />
       <div className="mx-auto max-w-[75ch]">
-        <h2 className="font-bold text-3xl mb-8">Your Cart</h2>
-        <div className="">
-          {cartItems.map((item) => (
-            <div key={item.id} className="py-4 flex justify-between">
-              <span>{item.name}</span><span><Currency />{item.price}</span>
-            </div>
-          ))}
+        <div className="flex justify-between items-center">
+          <h2 className="font-bold text-3xl mb-8">Your Cart</h2>
+          <p className="text-xl">{totalCartItems} items</p>
         </div>
-        <div className="flex justify-end py-8">
-          <h3 className="font-bold text-2xl">Total: <Currency />{totalCartPrice}</h3>
+        <div className="py-4 grid grid-cols-3 border-b">
+          <span>Item</span>
+          <span>Price</span>
+          <span></span>
+        </div>
+        {cartItems.map((item) => (
+          <div key={item.id} className="py-4 grid grid-cols-3">
+            <div>{item.name}</div>
+            <div><Currency />{item.price}</div>
+            <p onClick={() => removeFromCart(item.id)} className="underline">Remove</p>
+          </div>
+        ))}
+        <div className="py-4 grid grid-cols-3 border-t">
+          <span></span>
+          <h3 className="font-bold text-2xl"><Currency />{totalCartPrice}</h3>
+          <span></span>
         </div>
       </div>
     </main>
