8752a5f6d603505fdf1abfb04c82871ddda85cc9
Adam Gray <adam.gray@netlify.com>
2024-03-20 11:49:09 -0500

    blog ssr

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,20 +1,39 @@
 import { getPostBySlug } from "@/backend/get-posts-by-slug";
-import PostPage from "@/components/PostPage";
+import Nav from "@/components/Nav";
+import { cookies } from "next/headers";
+import Markdown from "react-markdown";
 
 export default async function Page(context: any) {
   const { slug } = context.params;
 
+  const locale = cookies().get("locale") || "en-US";
+
   const post = await getPostBySlug({
     source: "test",
-    locale: "en-US",
+    locale: locale as string,
     slug,
   });
 
-  if(!post) {
+  if (!post) {
     return {
       notFound: true,
     }
   }
 
-  return <PostPage post={post}/>
+  return (
+    <main>
+      <div className="container mx-auto px-8">
+        <Nav locale={locale as string} />
+      </div>
+
+      <h1 className="py-32 text-center bg-cover font-bold text-3xl text-white" style={{ backgroundImage: `url(/.netlify/images?url=/images/${post.slug}.jpg&fit=cover&w=2000&h=400)` }}>
+        {post.title}
+      </h1>
+
+      <div className='mx-auto max-w-[65ch] p-8'>
+        <article className="prose">
+          <Markdown>{post.content}</Markdown>
+        </article>
+      </div>
+    </main>)
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,34 @@
 import { getPosts } from '@/backend/get-posts';
-import BlogPage from '../components/BlogPage';
+import Nav from '@/components/Nav';
+import { cookies } from 'next/headers';
+import Link from 'next/link';
 
 export default async function Page() {
+  const locale = cookies().get("locale") || "en-US";
+
   const posts = await getPosts({
     source: "test",
-    locale: "en-US",
+    locale: locale as string,
   });
-  
-  return <BlogPage posts={posts}/>;
+
+  return (
+    <div>
+      <Nav locale={locale as string} />
+      <main>
+        <div className="container mx-auto px-8">
+        </div>
+        <div className="container mx-auto grid md:grid-cols-2 px-8">
+          <h1 className='font-bold text-4xl mb-8'>blog</h1>
+          <div>
+            {posts.map((post) => (
+              <Link key={post.id} href={`/blog/${post.slug}`} className='block mb-20 relative'>
+                <img src={`/.netlify/images?url=/images/${post.slug}.jpg`} alt={post.title} className='mb-4 rounded-xl shadow-xl' />
+                <h2 className='absolute bottom-0 px-8 py-16 bg-gradient-to-t from-gray-50 via-gray-50 text-2xl w-full'>{post.title}</h2>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
